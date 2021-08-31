c36523a2153f1f3bb8d9fd4a3ff6fb86dcb92710
kaniarahmah <kanianadya2003@gmail.com>
2021-08-31 10:54:43 +0700

    memasukan file

---
+++ JS/coba.js
@@ -0,0 +1,10 @@
+
+function hapus() {
+	confirm('apakah anda yakin ingin menghapus?');
+	document.write('ini sudah tehapus');
+}
+
+function simpan() {
+	alert('selamat data anda sudah tersimpan');
+	document.write('ini sudah tersimpan');
+}
